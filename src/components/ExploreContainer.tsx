f7b6886dd25a7c56afe15f9250b0226d91603676
Bryan Sharpley <motosharpley@gmail.com>
2022-06-26 12:18:52 -0700

    boiler plate cleanup

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,54 @@
+import {
+  IonSearchbar,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+} from '@ionic/react';
+import {
+  personCircle,
+  search,
+  star,
+  ellipsisHorizontal,
+  ellipsisVertical,
+  heart,
+} from 'ionicons/icons';
+import { useState } from 'react';
+
 import './ExploreContainer.css';
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [searchText, setSearchText] = useState('');
+
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonToolbar>
+      <IonButtons slot="secondary">
+        <IonButton>
+          <IonIcon slot="icon-only" icon={star} />
+        </IonButton>
+        <IonButton>
+          <IonIcon slot="icon-only" icon={heart} />
+        </IonButton>
+        <IonButton>
+          <IonIcon slot="icon-only" icon={personCircle} />
+        </IonButton>
+        <IonButton>
+          <IonIcon slot="icon-only" icon={ellipsisVertical} />
+        </IonButton>
+      </IonButtons>
+      <IonSearchbar
+        value={searchText}
+        onIonChange={(e) => setSearchText(e.detail.value!)}
+        animated
+        showClearButton="always"
+        color="dark"
+        inputMode="search"
+        type="search"
+        slot="start"
+      ></IonSearchbar>
+    </IonToolbar>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -7,13 +13,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Header</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Header</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />

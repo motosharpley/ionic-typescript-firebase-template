9fab9b911b631da4e8c3796ab92639c12b21b6f5
Bryan Sharpley <motosharpley@gmail.com>
2022-07-06 16:55:39 -0700

    wrap with ionpage for nav and ioncontent scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import { IonContent, IonPage } from '@ionic/react';
+
 const Home: React.FC = () => {
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <IonPage>
+      <IonContent>
+        <div>
+          <h2>Hello</h2>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
